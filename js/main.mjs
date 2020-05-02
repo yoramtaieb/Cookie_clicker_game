f1cc8224b5c8a1e3f80fafffad126f2a0ea64dd5
naimtahouri <naimtahouri@gmail.com>
2020-05-02 12:45:13 +0200

    update function

--- js/main.mjs
+++ js/main.mjs
@@ -15,42 +15,44 @@ const building = new Building()
 
 console.log(buildings)
 
+function CreateBuilding(){
+     // Ajout du nom de la boulangerie
+     bakeryName.innerHTML = myBakery._name
+     //  Ajout du nombre de cookies
+ cookiesStock.innerHTML = myBakery._cookies
+     //  Ajout du nombre de cookies par secondes
+ cookiePerSecond.innerHTML = myBakery._cookiesPerSecond
+     //  Boucle for permettant de récupérer le cost et le name dans DATA_MJS en mettant -3 car il y 5 éléments
+ for (let i = 0; i < buildings.length - 3; i++) {
+     // Création d'une div avec un id "building" + ajout du name et du cost dans ma div "building"
+     let div = document.createElement('div')
+     div.setAttribute('id', `building-${buildings[i].name}`.toLowerCase())
+         // Ajout de mes class "locked" et "disabled" à ma div building grâce à appendChild
+     div.classList.add('locked')
+     div.classList.add('disabled')
+     containerBuildings.appendChild(div)
+         // Création de ma div avec la class icon
+     let divIcon = document.createElement('div')
+     divIcon.setAttribute('class', 'icon')
+         // Utilisation de "backgroundPositionX" pour permettre d'afficher l'icon cursor
+     divIcon.style.backgroundPositionX = "-60px"
+         //   Création de ma div avec la class name 
+     let boxNameAndCost = document.createElement('div')
+     let divName = document.createElement('div')
+     divName.setAttribute('class', 'name')
+     divName.innerHTML = "???"
+     let divCost = document.createElement('div')
+     divCost.setAttribute('class', 'cost')
+     divCost.innerHTML = buildings[i].cost
+     let divNumber = document.createElement('div')
+     divNumber.setAttribute('class', 'number')
+     div.appendChild(divIcon)
+     boxNameAndCost.appendChild(divName)
+     boxNameAndCost.appendChild(divCost)
+     div.appendChild(boxNameAndCost)
+     div.appendChild(divNumber)
+ }
+}
+
 // Quand la page est chargée
-document.addEventListener("DOMContentLoaded", function() {
-    // Ajout du nom de la boulangerie
-    bakeryName.innerHTML = myBakery._name
-        //  Ajout du nombre de cookies
-    cookiesStock.innerHTML = myBakery._cookies
-        //  Ajout du nombre de cookies par secondes
-    cookiePerSecond.innerHTML = myBakery._cookiesPerSecond
-        //  Boucle for permettant de récupérer le cost et le name dans DATA_MJS en mettant -3 car il y 5 éléments
-    for (let i = 0; i < buildings.length - 3; i++) {
-        // Création d'une div avec un id "building" + ajout du name et du cost dans ma div "building"
-        let div = document.createElement('div')
-        div.setAttribute('id', `building-${buildings[i].name}`.toLowerCase())
-            // Ajout de mes class "locked" et "disabled" à ma div building grâce à appendChild
-        div.classList.add('locked')
-        div.classList.add('disabled')
-        containerBuildings.appendChild(div)
-            // Création de ma div avec la class icon
-        let divIcon = document.createElement('div')
-        divIcon.setAttribute('class', 'icon')
-            // Utilisation de "backgroundPositionX" pour permettre d'afficher l'icon cursor
-        divIcon.style.backgroundPositionX = "-60px"
-            //   Création de ma div avec la class name 
-        let boxNameAndCost = document.createElement('div')
-        let divName = document.createElement('div')
-        divName.setAttribute('class', 'name')
-        divName.innerHTML = "???"
-        let divCost = document.createElement('div')
-        divCost.setAttribute('class', 'cost')
-        divCost.innerHTML = buildings[i].cost
-        let divNumber = document.createElement('div')
-        divNumber.setAttribute('class', 'number')
-        div.appendChild(divIcon)
-        boxNameAndCost.appendChild(divName)
-        boxNameAndCost.appendChild(divCost)
-        div.appendChild(boxNameAndCost)
-        div.appendChild(divNumber)
-    }
-});
+document.addEventListener("DOMContentLoaded", CreateBuilding);
