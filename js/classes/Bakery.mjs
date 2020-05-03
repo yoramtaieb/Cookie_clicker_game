fa6f5da374f9a1ad44ae5d1038b160f10a4dd760
naimtahouri <naimtahouri@gmail.com>
2020-05-03 19:00:41 +0200

    Updating logiques statique en dynamique

--- js/classes/Bakery.mjs
+++ js/classes/Bakery.mjs
@@ -1,11 +1,11 @@
 class Bakery{
     constructor(name, cookies, buildings, cookiesPerClick, cookiesPerSecond){
-        this._name = "Le four"
-        this._cookies = 0
+        this._name = "⚡️ Le four ⚡️"
+        this._cookies = 1
+        this._buildings = []
         this._cookiesPerClick = 1
         this._cookiesPerSecond = 0
-        this._buildings = []
     }
-
 }
+
 export default Bakery
--- js/main.mjs
+++ js/main.mjs
@@ -1,5 +1,52 @@
-// Import modules & functions
-import CreateBuildings from "./functions/createBuildings.mjs"
+// Importation des classes et data
+import Bakery from './classes/Bakery.mjs'
+import { buildings } from './data.mjs'
 
-// Quand la page est chargée
-document.addEventListener("DOMContentLoaded", CreateBuildings);
+// Importation d'éléments HTML
+const containerBuildings = document.querySelector('#buildings')
+const bakeryName = document.querySelector('#bakery h2')
+const cookiesStock = document.querySelector('#cookiesStock span')
+const cookiesPerSecond = document.querySelector('#cookiesPerSecond span')
+
+// Instanciation des classes 
+const myBakery = new Bakery()
+
+function updatingValuesBakery(){
+    bakeryName.innerHTML = myBakery._name
+    cookiesStock.innerHTML = myBakery._cookies
+    cookiesPerSecond.innerHTML = myBakery._cookiesPerSecond
+}
+
+let iconPositionLocked = ['-65px 0px', '-65px -60px', '-65px -195px', '-65px -260px', '-65px -320px']
+
+let iconPositionEnabled = ['0px 0px', '-65px -60px', '-65px -195px', '-65px -260px', '-65px -320px']
+
+function renderTuils(index, name){
+    // Render Tuiles
+    containerBuildings.innerHTML +=
+    `<div id='building-${buildings[index].name.toLowerCase()}' class='locked disabled'>
+    <div class='icon'></div>
+    <div class="boxInfos">
+        <div class='name'>${buildings[index].name}</div>
+        <div class='cost'>${buildings[index].cost}</div>
+    </div>
+    <div class='number'></div>
+    </div>`
+
+    document.getElementsByClassName('icon')[index].style.backgroundPosition = iconPositionLocked[index]
+    const priceTuil = document.getElementsByClassName('cost')
+
+    // Update Tuiles en fonctions de l'argent en poche
+    if(myBakery._cookies > parseInt(priceTuil[index].innerHTML)){
+        let tuil = document.getElementById(`building-${[name]}`).classList.remove('disabled')
+        let iconEnabled = document.getElementsByClassName('icon')[index].style.backgroundPosition = iconPositionEnabled[index]
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updatingValuesBakery()
+    renderTuils(0, 'cursor')
+    renderTuils(1, 'grandma')
+    renderTuils(2, 'farm')
+    renderTuils(3, 'mine')
+})
