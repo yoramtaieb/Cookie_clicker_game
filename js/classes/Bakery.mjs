61635d814b9307ea3c2a93babb4843e881dd4ce5
naimtahouri <naimtahouri@gmail.com>
2020-05-03 23:32:10 +0200

    updating bakeCookies function & click on cookie + incrementation du stock

--- js/classes/Bakery.mjs
+++ js/classes/Bakery.mjs
@@ -6,6 +6,12 @@ class Bakery {
         this._cookiesPerClick = 1
         this._cookiesPerSecond = 0
     }
+    
+    // Méthode incrémentant le nombre de cookies en stock
+    bakeCookies(howMany){
+        howMany = this._cookiesPerClick
+       return this._cookies += howMany
+    }
 }
 
 export default Bakery
--- js/main.mjs
+++ js/main.mjs
@@ -7,6 +7,8 @@ const containerBuildings = document.querySelector('#buildings')
 const bakeryName = document.querySelector('#bakery h2')
 const cookiesStock = document.querySelector('#cookiesStock span')
 const cookiesPerSecond = document.querySelector('#cookiesPerSecond span')
+const bigCookie = document.getElementById('bigCookie')
+
 
 // Instanciation des classes 
 const myBakery = new Bakery()
@@ -34,14 +36,33 @@ function renderTuils(index, name) {
     </div>`
 
     document.getElementsByClassName('icon')[index].style.backgroundPosition = iconPositionLocked[index]
-    const priceTuil = document.getElementsByClassName('cost')
+    // Passage des paramètres index et name de renderTuils
+    UpdatingTuils(index, name)
+}
 
+function UpdatingTuils(index, name){
+    const priceTuil = document.getElementsByClassName('cost')
     // Update Tuiles en fonctions de l'argent en poche
     if (myBakery._cookies >= parseInt(priceTuil[index].innerHTML)) {
+        // Paramètre name pour générer l'id dynamiquement afin de remove la class "disabled"
         let tuil = document.getElementById(`building-${[name]}`).classList.remove('disabled')
         let iconEnabled = document.getElementsByClassName('icon')[index].style.backgroundPosition = iconPositionEnabled[index]
     }
 }
+
+
+// Evènements 
+bigCookie.addEventListener('click', () => {
+    updatingValuesBakery()
+    console.log(myBakery._cookies)
+    myBakery.bakeCookies()
+    UpdatingTuils(0, 'cursor')
+    UpdatingTuils(1, 'grandma')
+    UpdatingTuils(2, 'farm')
+    UpdatingTuils(3, 'mine')
+    UpdatingTuils(4, 'factory')
+})
+
 
 document.addEventListener('DOMContentLoaded', () => {
     updatingValuesBakery()
@@ -50,5 +71,4 @@ document.addEventListener('DOMContentLoaded', () => {
     renderTuils(2, 'farm')
     renderTuils(3, 'mine')
     renderTuils(4, 'factory')
-
 })
